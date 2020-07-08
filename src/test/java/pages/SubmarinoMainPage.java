bdc902fb9c963cc922f5f701150483bfa01e5966
Rodrigo Souza <recsouza@gmail.com>
2020-07-08 02:33:52 -0300

    Adicionando seletor
    Adicionando seletor para o botao de cadastro de usuario

--- src/test/java/pages/SubmarinoMainPage.java
+++ src/test/java/pages/SubmarinoMainPage.java
@@ -9,6 +9,7 @@ public class SubmarinoMainPage {
     private WebDriver driver;
 
     private By favoriteBtn = By.cssSelector("#h_wishlist .wish-link");
+    private By registerBtn = By.cssSelector(".usr-actions .usr-signup");
 
     public SubmarinoMainPage(WebDriver driver){
         this.driver = driver;
@@ -21,5 +22,9 @@ public class SubmarinoMainPage {
 
     public void openFavoriteWishList(){
         driver.findElement(favoriteBtn).click();
+    }
+
+    public void openNewUserForm() {
+        driver.findElement(registerBtn).click();
     }
 }
