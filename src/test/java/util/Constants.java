7e816924658ca602f1e4b04add71e2b155ba4323
Alex Candido <alexcandido10@gmail.com>
2020-07-11 02:01:11 -0300

    Adding submarino account constants

--- src/test/java/util/Constants.java
+++ src/test/java/util/Constants.java
@@ -3,5 +3,8 @@ package util;
 public class Constants {
 
     public static final String SUBMARINO_MAIN_URL = "https://www.submarino.com.br";
+    public static final String SUBMARINO_ACCOUNT = "testdesistemas20@gmail.com";
+    public static final String SUBMARINO_PASSWORD = "r3m3mb3r";
+    public static final String ACCOUNT_FIRST_NAME = "Joao";
 
 }
