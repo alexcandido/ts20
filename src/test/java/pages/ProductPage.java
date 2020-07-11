3b1dffa29ae076cac2d163338e6db597193b99f7
Alex Candido <alexcandido10@gmail.com>
2020-07-11 02:09:38 -0300

    Adding ProductPage class

---
+++ src/test/java/pages/ProductPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import manager.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import util.CommonActions;
+
+public class ProductPage {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private CommonActions commonActions;
+
+    public By favoriteIcon = By.xpath("//div[contains(@class,'favorite-button')]");
+
+    public ProductPage(WebDriver driver){
+        this.driver = driver;
+        wait = DriverFactory.getWait();
+        commonActions = new CommonActions(driver);
+    }
+    public void addProductToFavoriteList(){
+        commonActions.click(favoriteIcon);
+    }
+
+}
