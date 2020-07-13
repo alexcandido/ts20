4a09e07de580368352bfeceb14605f19b9bba3e8
Alex Candido <alexcandido10@gmail.com>
2020-07-12 23:35:34 -0300

    Implementing pending methods of FavoritePage

--- src/test/java/pages/FavoritePage.java
+++ src/test/java/pages/FavoritePage.java
@@ -4,23 +4,40 @@ import manager.DriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import util.CommonActions;
 
 public class FavoritePage {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private CommonActions commonActions;
 
-    private By emptyWishList = By.xpath("//div[@id='content']//span[contains(text(),'Você não possui produtos salvos')]");
+    private By emptyWishListMessage = By.xpath("//div[@id='content']//span");
     private By favoriteIcon = By.xpath("//div[contains(@class,'TouchableFavorite')]");
+    private By productCard = By.xpath("//div[contains(@class,'product-card-photo')]");
+    private By productName = By.xpath("//h2[contains(@class,'TitleUI')]");
+
 
     public FavoritePage(WebDriver driver){
         this.driver = driver;
         wait = DriverFactory.getWait();
+        commonActions = new CommonActions(driver);
     }
 
     public void clearWishList(){
-        while (driver.findElement(favoriteIcon).isDisplayed()){
-            driver.findElement(favoriteIcon).click();
+        commonActions.waitForAnElementBeVisible(favoriteIcon);
+
+        while (!commonActions.findElement(emptyWishListMessage).getText().equals("Você não possui produtos salvos")){
+            commonActions.findElement(favoriteIcon).click();
         }
+    }
+
+    public String getProductName(){
+        commonActions.waitForAnElementBeVisible(productCard);
+        return commonActions.findElement(productName).getText();
+    }
+
+    public String getEmptyWishListMessage(){
+        return commonActions.findElement(emptyWishListMessage).getText();
     }
 }
